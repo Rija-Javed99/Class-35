8613922e919f2f422c1cc3249641cc4b56fba418
Rija-Javed99 <73756430+Rija-Javed99@users.noreply.github.com>
2021-06-06 11:17:05 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,15 @@
-var hypnoticBall, database;
+var hypnoticBall;
 var position;
 
 
-function setup(){
-  database = firebase.database();
+function setup()
+{
   console.log(database);
   createCanvas(500,500);
 
   hypnoticBall = createSprite(250,250,10,10);
   hypnoticBall.shapeColor = "red";
 
-
-  var hypnoticBallPosition = database.ref('ball/position');
-  hypnoticBallPosition.on("value", readPosition, showError);
 }
 
 function draw(){
@@ -35,19 +32,13 @@ function draw(){
 }
 
 function writePosition(x,y){
-  database.ref('ball/position').set({
-    'x': position.x + x ,
-    'y': position.y + y
-  })
+ 
 }
 
 function readPosition(data){
-  position = data.val();
-  console.log(position.x);
-  hypnoticBall.x = position.x;
-  hypnoticBall.y = position.y;
+ 
 }
 
 function showError(){
-  console.log("Error in writing to the database");
+  
 }
